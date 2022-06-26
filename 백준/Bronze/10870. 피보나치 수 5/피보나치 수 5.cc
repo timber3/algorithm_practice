b2926824ff95c1b4ab004c96f139c5ea1f8aa005
timber3 <75405129+timber3@users.noreply.github.com>
2022-06-26 22:35:05 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	int arr[21];
+
+	arr[0] = 0;
+	arr[1] = 1;
+
+	for (int i = 2; i < 21; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	cout << arr[n];
+
+	return 0;
+}

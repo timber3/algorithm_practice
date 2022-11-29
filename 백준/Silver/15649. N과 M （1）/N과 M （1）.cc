6786923d536d7da23dc424a4ddba71cfe6d56d25
timber3 <75405129+timber3@users.noreply.github.com>
2022-11-29 16:56:02 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,33 +1,39 @@
 #include <iostream>
-#define MAX 9
+#include <algorithm>
+
 using namespace std;
 
-int n,m;
-int arr[MAX] = {0,};
-bool visited[MAX] = {0,};
+int N, M;
+int result[9];
+int visited[9] = { 0, };
 
-void dfs(int cnt)
+void DFS(int cnt)
 {
-    if(cnt == m)
-    {
-        for(int i = 0; i < m; i++)
-            cout << arr[i] << ' ';
-        cout << '\n';
-        return;
-    }
-    for(int i = 1; i <= n; i++)
-    {
-        if(!visited[i])
-        {
-            visited[i] = true;
-            arr[cnt] = i;
-            dfs(cnt+1);
-            visited[i] = false;
-        }
-    }
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << result[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = 1; // true
+			result[cnt] = i;
+			DFS(cnt + 1);
+			visited[i] = 0; // false
+		}
+	}
 }
 
-int main() {
-    cin >> n >> m;
-    dfs(0);
+int main()
+{
+	cin >> N >> M;
+	DFS(0);
+	return 0;
 }

a23c05be1d17b8916cd44667911f8c00082fefa3
timber3 <75405129+timber3@users.noreply.github.com>
2022-12-14 21:37:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -4,49 +4,32 @@
 
 using namespace std;
 
-/// <summary>
-/// 
-///		BFS 로 푸는 문제이나
-///		다음 노드로 이동할 때 간선의 가중치가 존재한다.
-/// 
-///		순간이동은 가중치가 0 걷기는 가중치가 1
-/// 
-///		BFS는 가중치가 존재하는 순간 최단거리를 구할 수 없게 되기 때문에
-///		0-1 BFS 를 사용하거나 다익스트라를 통한 문제 풀이를 해야한다.
-/// 
-///		여기서는 deqeue 를 사용한 0-1 BFS를 사용하였다.
-/// </summary>
-/// 
-int n;
-int k;
+int n, k;
+int cnt;
 
-int result[100001] = { 0, };
-
-// visited[] 는 체크를 하기 위함 ( BFS 에서만 )
-// visited[] 대신 distance를 사용함
 int dist[100001];
 
-vector<int> par(100001);
-
-struct node {
+struct node
+{
 	int cur;
+	int cnt;
 };
 
 void bfs()
 {
-	deque<node> dq;
-	dq.push_back({ n });
+	deque<node> q;
+	q.push_back({ n,0 });
 
-	fill(dist, dist + sizeof(dist)/4, 999999);
-
-	// 현재 거리 초기화
+	fill(dist, dist + sizeof(dist) / sizeof(int), 999999);
 	dist[n] = 0;
 
-	while (!dq.empty())
-	{
-		int cur = dq.front().cur;
 
-		dq.pop_front();
+	while (!q.empty())
+	{
+		int cur = q.front().cur;
+		int cnt = q.front().cnt;
+
+		q.pop_front();
 
 		if (cur == k)
 		{
@@ -55,26 +38,25 @@ void bfs()
 		}
 
 		// 순간이동
-		if (cur * 2 < 100001 && dist[cur*2] > dist[cur])
+		if (cur * 2 <= 100001 && dist[cur * 2] > dist[cur])
 		{
-			dq.push_front({ cur * 2 });
 			dist[cur * 2] = dist[cur];
+			q.push_front({ cur * 2, cnt });
 		}
 
 		// 앞으로 걷기
 		if (cur + 1 <= 100001 && dist[cur + 1] > dist[cur] + 1)
 		{
-			dq.push_back({ cur + 1});
-			dist[cur + 1] = dist[cur] + 1;
+			dist[cur + 1 ] = dist[cur] + 1;
+			q.push_back({ cur + 1, cnt+1 });
 		}
 
 		// 뒤로 걷기
 		if (cur - 1 >= 0 && dist[cur - 1] > dist[cur] + 1)
 		{
-			dq.push_back({ cur - 1 });
 			dist[cur - 1] = dist[cur] + 1;
+			q.push_back({ cur-1 , cnt+1 });
 		}
-
 	}
 }
 

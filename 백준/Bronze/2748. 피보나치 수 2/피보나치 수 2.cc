843a1fa593a4473fd5e7a8d3c522e68559a29124
timber3 <75405129+timber3@users.noreply.github.com>
2023-02-24 10:48:41 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+long long arr[21];
+
+int main()
+{
+	cin >> n;
+
+	arr[0] = 0;
+	arr[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	cout << arr[n];
+
+	return 0;
+}

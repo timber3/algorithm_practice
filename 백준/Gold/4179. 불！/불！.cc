8272dad565e591cbbb07965ba3a036f3e557d8f8
timber3 <75405129+timber3@users.noreply.github.com>
2023-03-15 23:41:57 +0900

    [Gold IV] Title: 불!, Time: 80 ms, Memory: 10832 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,107 +1,112 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
+
 using namespace std;
-int R, C;
-char graph[1000][1000];
-bool visited[1000][1000];
-int fire_map[1000][1000];
-vector<pair<int, int>> fire;
-int groupCnt = 0;
-int dir[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
 
-void make_fireMap() {
-	vector<queue<pair<int, int>>> fireQ(fire.size());
+char Map[1001][1001];
+int dist[1001][1001];
+int fdist[1001][1001];
 
-	for (int i = 0; i < fire.size(); i++) {
-		fireQ[i].push({ fire[i].first,fire[i].second });
-	}
+int r, c;
 
-	for (int f = 0; f < fireQ.size(); f++) {
-		while(!fireQ[f].empty()) {
-			int cur_fireR = fireQ[f].front().first;
-			int cur_fireC = fireQ[f].front().second;
-			fireQ[f].pop();
+int dx[] = { 0,0,-1,1 };
+int dy[] = { 1,-1,0,0 };
 
-			for (int i = 0; i < 4; i++) {
-				int	next_fireR = cur_fireR + dir[i][0];
-				int	next_fireC = cur_fireC + dir[i][1];
-				if (next_fireR >= 0 && next_fireR < R && next_fireC >= 0 && next_fireC < C && graph[next_fireR][next_fireC] != '#') {
-					if (fire_map[cur_fireR][cur_fireC] + 1 < fire_map[next_fireR][next_fireC]) {
-						fireQ[f].push({ next_fireR,next_fireC });
-						fire_map[next_fireR][next_fireC] = fire_map[cur_fireR][cur_fireC] + 1;
-					}
-				}
-				
+typedef struct node {
+	int x;
+	int y;
+};
+
+queue<node> fq;
+queue<node> q;
+
+int bfs()
+{
+	while (!fq.empty())
+	{
+		int cx = fq.front().x;
+		int cy = fq.front().y;
+
+		fq.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= r || ny >= c || Map[nx][ny] == '#' || fdist[nx][ny] > -1)
+				continue;
+			else
+			{
+				fq.push({ nx, ny });
+				fdist[nx][ny] = fdist[cx][cy] + 1;
 			}
+
 		}
 	}
+
+	while (!q.empty())
+	{
+		int cx = q.front().x;
+		int cy = q.front().y;
+
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= r || ny >= c)
+				return dist[cx][cy] + 1;
+
+			if (Map[nx][ny] == '#' || dist[nx][ny] > -1)
+				continue;
+			if (fdist[nx][ny] > -1 && fdist[nx][ny] <= dist[cx][cy] + 1)
+				continue;
+			
+			dist[nx][ny] = dist[cx][cy] + 1;
+
+			q.push({ nx,ny });
+		}
+	}
+
+	return -1;
 }
-void move(int startR, int startC) {
-	queue<pair<pair<int,int>,int>> startQ;
-	
-	startQ.push({ { startR,startC },0 });
-	
 
-	while (!startQ.empty()) {
+int main()
+{
+	cin >> r >> c;
 
-		int cur_startR = startQ.front().first.first;
-		int cur_startC = startQ.front().first.second;
-		int cnt = startQ.front().second;
+	for (int i = 0; i < r; i++)
+	{
+		string str;
+		cin >> str;
 
-		if (cur_startR == 0 || cur_startR == R-1 || cur_startC == 0 || cur_startC == C-1) {//나갔을 때
-			cout << cnt + 1;
-			return;
-		}
-		startQ.pop();
-		for (int i = 0; i < 4; i++) {
-			int next_startR = cur_startR + dir[i][0];
-			int next_startC = cur_startC + dir[i][1];
-			if (next_startR >= 0 && next_startR < R&&next_startC >= 0 && next_startC < C) {
-				if (fire_map[next_startR][next_startC] > cnt + 1 && graph[next_startR][next_startC] == '.') {
-					if (!visited[next_startR][next_startC]) {
-						startQ.push({ { next_startR,next_startC },cnt + 1 });
-						visited[next_startR][next_startC] = true;
-					}
-				}
+		fill(dist[i], dist[i] + c, -1);
+		fill(fdist[i], fdist[i] + c, -1);
+
+		for (int j = 0; j < str.size(); j++)
+		{
+			Map[i][j] = str[j];
+			if (Map[i][j] == 'J')
+			{
+				q.push({ i,j });
+				dist[i][j] = 0;
 			}
-
-		}
-	}
-
-
-	cout << "IMPOSSIBLE";
-	return ;
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	cin >> R >> C;
-	
-	pair<int,int>start;
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			char ch;
-			cin >> ch;
-			if (ch == 'F') {
-				fire.push_back({ i,j });
-				graph[i][j] = 'F';
-				fire_map[i][j] = 0;
-				
-			}
-			else if (ch == 'J') {
-				start = { i,j };
-				graph[i][j] = '.';
-				fire_map[i][j] = 987654321;
-			}
-			else {
-				graph[i][j] = ch;
-				fire_map[i][j] = 987654321;
+			if (Map[i][j] == 'F')
+			{
+				fq.push({ i,j });
+				fdist[i][j] = 0;
 			}
 		}
 	}
-	make_fireMap();
-	move(start.first, start.second);
+
+	int result = bfs();
+
+	if (result == -1)
+		cout << "IMPOSSIBLE";
+	else
+		cout << result;
+
 	return 0;
 }

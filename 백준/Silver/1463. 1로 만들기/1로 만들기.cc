8ba67a6713cd7d693e69d5bcc56b812f04906b8e
timber3 <75405129+timber3@users.noreply.github.com>
2023-03-24 22:03:58 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,55 +1,32 @@
 #include <bits/stdc++.h>
 
+#define ll long long
+
 using namespace std;
 
-int n;
+ll n;
+int dp[1000001];
 
-int dp[1000000] = { 0, };
+// DP(12) = DP(4) + 1
+// DP(12) = DP(6) + 1
+// DP(12) = DP(11) + 1
+
+// => DP(12) = min(DP(4) + 1, DP(6) + 1 , DP(11) + 1)
+// => DP(k) = min(DP(k/3) + 1 , DP(k/2) + 1, DP(k-1) + 1)
 
 int main()
 {
 	cin >> n;
 
-	// 1. 3으로 나눈다.
-	// 2. 2로 나눈다.
-	// 3. 1을 뺀다.
-
-	// 결과로 1을 만들어 내야한다.
-
 	dp[1] = 0;
-	dp[2] = 1;
-	dp[3] = 1;
-	dp[4] = 2;
 
-	for (int i = 5; i <= n; i++)
+	for (int i = 2; i <= n; i++)
 	{
-		int div2 = 0;
-		int div3 = 0;
-
+		dp[i] = dp[i - 1] + 1;
 		if (i % 2 == 0)
-			div2 = 1;
+			dp[i] = min(dp[i], dp[i / 2] + 1);
 		if (i % 3 == 0)
-			div3 = 1;
-
-		if (!(div2 && div3))
-		{
-			if (div2)
-			{
-				dp[i] = min(dp[i / 2] + 1, dp[i-1] +1);
-			}
-			else if (div3)
-			{
-				dp[i] = min(dp[i / 3] + 1, dp[i - 1] + 1);
-			}
-			else
-			{
-				dp[i] = dp[i - 1] + 1;
-			}
-		}
-		else
-		{
-			dp[i] = min(dp[i / 2] + 1, dp[i / 3] + 1);
-		}
+			dp[i] = min(dp[i], dp[i / 3] + 1);
 	}
 
 	cout << dp[n];

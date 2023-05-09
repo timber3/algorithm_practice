2fef16391d32dd1db49e62c1f8da8bc30937dc98
SH_OH <75405129+timber3@users.noreply.github.com>
2023-05-09 22:22:26 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -10,41 +10,31 @@ int main()
 {
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j <= i; j++)
+		for (int j = 1; j <= i; j++)
 		{
 			cin >> Map[i][j];
 		}
 	}
 	// 초기값 설정
 
-	dp[0][0] = Map[0][0];
+	dp[1][1] = Map[1][1];
 
-	for (int i = 1; i < n; i++)
+	for (int i = 2; i <= n; i++)
 	{
-		dp[i][i] = dp[i - 1][i - 1] + Map[i][i];
-	}
-
-	for (int i = 1; i < n; i++)
-	{
-		dp[i][0] = dp[i - 1][0] + Map[i][0];
-	}
-
-	for (int i = 2; i < n; i++)
-	{
-		for (int j = 1; j < i; j++)
+		for (int j = 1; j <= i; j++)
 		{
-			dp[i][j] = max(dp[i - 1][j-1], dp[i-1][j]) + Map[i][j];
+			dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + Map[i][j];
 		}
 	}
 
 	int Max = 0;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		if (Max < dp[n - 1][i])
-			Max = dp[n - 1][i];
+		if (Max < dp[n][i])
+			Max = dp[n][i];
 	}
 
 	cout << Max;

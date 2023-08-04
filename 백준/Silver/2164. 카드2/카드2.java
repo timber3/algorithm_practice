a829001c7da07967365d8ebb590cd89d4e2c9e33
SH_OH <75405129+timber3@users.noreply.github.com>
2023-08-04 17:18:19 +0900

    [Silver IV] Title: 카드2, Time: 288 ms, Memory: 41840 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	static int arr[];
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		arr = new int[t];
+		
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		
+		for (int i = 1 ; i <= t ; i ++) {
+			queue.add(i);
+		}
+		
+		while (queue.size() != 1) {
+			queue.pollFirst();
+			if (queue.size() == 1)
+				break;
+			int v = queue.pollFirst();
+			queue.addLast(v);			
+		}
+		
+		int result = queue.poll();
+		
+		System.out.println(result);
+
+	}
+
+}

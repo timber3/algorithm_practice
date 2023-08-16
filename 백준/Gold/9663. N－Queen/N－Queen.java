d0d8d86e096480b48136bc05aa5be288bd07b01d
SH_OH <75405129+timber3@users.noreply.github.com>
2023-08-16 14:47:08 +0900

    [Gold IV] Title: N-Queen, Time: 2568 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -9,29 +9,24 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int n, sum;
 	static int[][] Map;
-	static int[] col;
+	static boolean[] col;
 	static boolean[] rightDown, rightUp;
 	
 	public static void main(String[] args) throws IOException {
 		n = Integer.parseInt(br.readLine());
 		
 		Map = new int[n+1][n+1];
-		col = new int[n+1];
+		col = new boolean[n+1];
 		rightDown = new boolean[2*n];
 		rightUp = new boolean[2*n];
 		
-		solve(1, 0);
+		solve(1);
 		
 		System.out.println(sum);
 
 	}
 	
-	static void solve(int row, int queens) {
-		
-		// 둔 자리가 성립하는지 검사
-		if (!available(row-1)) {
-			return;
-		}
+	static void solve(int row) {
 		
 		// 다 뒀다면
 		if (row > n) {
@@ -41,31 +36,21 @@ public class Main {
 		
 		// 퀸 두기
 		for (int i = 1 ; i <= n ; i ++) {
-			col[row] = i;
-			
-			if(rightDown[row - col[row] + n] || rightUp[row + col[row] -1]) {
+			if(col[i] || rightDown[row - i + n] || rightUp[row + i -1]) {
 				continue;
 			}
 			
-			rightDown[(row - col[row]) + n] = true;  
-			rightUp[row + col[row] -1] = true;
+			col[i] = true;
+			rightDown[(row - i) + n] = true;  
+			rightUp[row + i -1] = true;
 			
-			solve(row + 1, queens + 1);
+			solve(row + 1);
 			
+			col[i] = false; 
 			rightDown[(row - i) + n] = false;  
 			rightUp[i + row -1] = false; 
 		}
 		
 	}
 	
-	static boolean available(int row) {
-		
-		for (int i = 1 ; i < row ; i ++) {
-			if (col[i] == col[row])
-				return false;
-		}
-		
-		return true;
-	}
-
 }

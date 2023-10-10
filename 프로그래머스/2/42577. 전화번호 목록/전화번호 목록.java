55c70046f0d0676a5b5ceea45129446d112e1f15
SH_OH <75405129+timber3@users.noreply.github.com>
2023-10-10 16:54:59 +0900

    [level 2] Title: 전화번호 목록, Time: 302.74 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,20 +3,31 @@ import java.io.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+         boolean answer = true;
         
-        HashMap<String, Integer> map = new HashMap<>();
+//         HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i = 0 ; i < phone_book.length ; i ++) {
-            map.put(phone_book[i], i);
-        }
+//         for (int i = 0 ; i < phone_book.length ; i ++) {
+//             map.put(phone_book[i], i);
+//         }
         
-        for (int i = 0 ; i < phone_book.length; i++) {
-            for (int j = 1 ; j < phone_book[i].length(); j++) {
-                if(map.containsKey(phone_book[i].substring(0,j))) {
-                    answer = false;
-                    return answer;
-                }
+//         for (int i = 0 ; i < phone_book.length; i++) {
+//             for (int j = 1 ; j < phone_book[i].length(); j++) {
+//                 if(map.containsKey(phone_book[i].substring(0,j))) {
+//                     answer = false;
+//                     return answer;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+        
+        Arrays.sort(phone_book);
+
+        for(int i = phone_book.length - 1 ; i > 0; i --) {
+            if (phone_book[i].startsWith(phone_book[i-1])) {
+                answer = false;
+                return answer;
             }
         }
         

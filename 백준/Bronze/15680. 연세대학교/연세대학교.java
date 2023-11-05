0455a6db1b87b8202712fcbd273bf7e0d8994067
SH_OH <75405129+timber3@users.noreply.github.com>
2023-11-06 08:56:02 +0900

    [Bronze V] Title: 연세대학교, Time: 72 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		if(N == 0) {
+			sb.append("YONSEI");
+		}
+		else {
+			sb.append("Leading the Way to the Future");
+		}
+		System.out.print(sb);
+	}
+}

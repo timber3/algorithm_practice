50e32f560a4d1db6daffc216535fe7a5bb7ffdab
SH_OH <75405129+timber3@users.noreply.github.com>
2023-11-20 04:59:51 +0900

    [Silver IV] Title: 스택, Time: 148 ms, Memory: 17024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,41 +6,44 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Stack<Integer> stackInt = new Stack<>();
 		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<N;i++) {
 			String s = br.readLine();
 			StringTokenizer str = new StringTokenizer(s);
 			String st = str.nextToken();
-			if(st.equals("push")) {
-				int A = Integer.parseInt(str.nextToken());
-				stackInt.push(A);
-			}
-			else if(st.equals("pop")) {
-				if(stackInt.size() == 0) {
-					System.out.println("-1");
-				}
-				else {
-					System.out.println(stackInt.pop());
-				}
-			}
-			else if(st.equals("size")) {
-				System.out.println(stackInt.size());
-			}
-			else if(st.equals("empty")) {
-				if(stackInt.empty()) {
-					System.out.println("1");
-				}
-				else {
-					System.out.println("0");
-				}
-			}
-			else if(st.equals("top")) {
-				if(stackInt.size() == 0) {
-					System.out.println("-1");
-				}
-				else {
-					System.out.println(stackInt.peek());
-				}
+			switch(st) {
+				case "push":
+					int A = Integer.parseInt(str.nextToken());
+					stackInt.push(A);
+					break;
+				case "pop":
+					if(stackInt.isEmpty()) {
+						sb.append(-1 + "\n");
+					}
+					else {
+						sb.append(stackInt.pop() + "\n");
+					}
+					break;
+				case "size":
+					sb.append(stackInt.size() + "\n");
+					break;
+				case "empty":
+					if(stackInt.isEmpty()) {
+						sb.append(1 + "\n");
+					}
+					else {
+						sb.append(0 + "\n");
+					}
+					break;
+				case "top" :
+					if(stackInt.isEmpty()) {
+						sb.append(-1 + "\n");
+					}
+					else {
+						sb.append(stackInt.peek() + "\n");
+					}
 			}
 		}
+		System.out.println(sb);
 	}
 }

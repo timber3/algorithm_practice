e4acdd8ceeed42ddeae595bf873fcccc3bbb8167
SH_OH <75405129+timber3@users.noreply.github.com>
2023-11-20 05:18:12 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 448 ms, Memory: 79136 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -5,15 +5,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int A = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<A;i++) {
-			StringBuilder sb = new StringBuilder();
 			String Sentence = br.readLine();
 			StringTokenizer str = new StringTokenizer(Sentence);
 			while(str.hasMoreTokens()) {
 				StringBuilder sb2 = new StringBuilder(str.nextToken()).reverse();
 				sb.append(sb2.toString() + " ");
 			}
-			System.out.println(sb);
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 }

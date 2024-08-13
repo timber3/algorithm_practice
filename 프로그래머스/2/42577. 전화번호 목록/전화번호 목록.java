2b92d93f1916a20526954b4e6862742cd5547166
오승현 <75405129+timber3@users.noreply.github.com>
2024-08-13 13:28:20 +0900

    [level 2] Title: 전화번호 목록, Time: 606.68 ms, Memory: 253 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,35 +2,21 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-         boolean answer = true;
+    public boolean solution(String[] phone_book) throws Exception{
         
-//         HashMap<String, Integer> map = new HashMap<>();
-        
-//         for (int i = 0 ; i < phone_book.length ; i ++) {
-//             map.put(phone_book[i], i);
-//         }
-        
-//         for (int i = 0 ; i < phone_book.length; i++) {
-//             for (int j = 1 ; j < phone_book[i].length(); j++) {
-//                 if(map.containsKey(phone_book[i].substring(0,j))) {
-//                     answer = false;
-//                     return answer;
-//                 }
-//             }
-//         }
-        
-//         return answer;
+        HashSet<String> set = new HashSet<>();
         
         Arrays.sort(phone_book);
-
-        for(int i = phone_book.length - 1 ; i > 0; i --) {
-            if (phone_book[i].startsWith(phone_book[i-1])) {
-                answer = false;
-                return answer;
+        
+        for (String s : phone_book) {
+            for (int i = 0 ; i <= s.length(); i ++) {
+                if (set.contains(s.substring(0,i))) {
+                    return false;
+                }
             }
+            set.add(s);
         }
         
-        return answer;
+        return true;
     }
 }

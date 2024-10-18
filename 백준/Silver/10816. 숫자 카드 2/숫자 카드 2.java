4e70a52e4b8f52123197242010e4729769747186
오승현 <75405129+timber3@users.noreply.github.com>
2024-10-19 01:35:50 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1608 ms, Memory: 243912 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,22 +2,60 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int[] array = new int [20000001];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			int A = Integer.parseInt(st.nextToken());
-			array[A+10000000]++;
-		}
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<M;i++) {
-			int B = Integer.parseInt(st.nextToken());
-			sb.append(array[B+10000000] + " ");
-		}
-		System.out.print(sb);
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[] arr;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < n ; i ++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (map.containsKey(arr[i])) {
+                int temp = map.get(arr[i]);
+                map.put(arr[i], temp+1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        Arrays.sort(arr);
+
+        for (int i = 0 ; i < m ; i ++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            int left = 0;
+            int right = arr.length - 1;
+
+            while(left <= right) {
+                int mid = (left + right) / 2;
+
+                if (target == arr[mid]) {
+                    sb.append(map.get(arr[mid])).append(" ");
+                    break;
+                }
+
+                if (target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            if (left > right) sb.append(0).append(" ");
+
+        }
+
+        System.out.println(sb);
+    }
 }

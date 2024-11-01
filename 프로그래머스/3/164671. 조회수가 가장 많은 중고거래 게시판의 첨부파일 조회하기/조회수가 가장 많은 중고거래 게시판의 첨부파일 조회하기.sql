7a689f777c508906239c1f825bd73a09c814a5c7
오승현 <75405129+timber3@users.noreply.github.com>
2024-11-01 23:43:54 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,19 +1,17 @@
-# select concat('/home/grep/src/', a.BOARD_ID , '/' , a.FILE_ID , a.FILE_NAME , a.FILE_EXT)
-# from USED_GOODS_FILE a, (
-#     select Max(VIEWS)
-#     from USED_GOODS_BOARD
-# ) b
-# where a.BOARD_ID = b.BOARD_ID
-# order by a.FILE_ID
-
-select concat('/home/grep/src/', a.BOARD_ID , '/' , FILE_ID , FILE_NAME , FILE_EXT)
+select
+    concat('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT)
 from
-    (select BOARD_ID
-     from USED_GOODS_BOARD  
-     where VIEWS = (
-         select Max(VIEWS)
-         from USED_GOODS_BOARD 
-     )
-    ) a, USED_GOODS_FILE  as b
-where a.BOARD_ID = b.BOARD_ID
+    USED_GOODS_FILE  
+where
+    BOARD_ID in (
+        select
+            *
+        from
+            (select
+                BOARD_ID
+             from
+                USED_GOODS_BOARD
+             order by views desc limit 1
+            ) as t
+    )
 order by FILE_ID desc

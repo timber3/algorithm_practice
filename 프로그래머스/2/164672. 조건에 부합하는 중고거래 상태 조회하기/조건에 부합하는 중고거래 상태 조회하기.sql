6db30dafb82e688d4699aae7c4b4dbed4d457e6d
오승현 <75405129+timber3@users.noreply.github.com>
2024-11-02 00:58:10 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,6 +1,13 @@
--- 코드를 입력하세요
-SELECT BOARD_ID , WRITER_ID , TITLE , PRICE , 
-(case STATUS when 'DONE' then '거래완료' when 'RESERVED' then '예약중' else '판매중' end) as STATUS
+select
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    case 
+        when STATUS = 'SALE' then '판매중'
+        when STATUS = 'RESERVED' then '예약중'
+        when STATUS = 'DONE' then '거래완료'
+    end as status
 from USED_GOODS_BOARD 
-where CREATED_DATE = '2022-10-05'
-order by BOARD_ID desc;
+where DATE_FORMAT(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
+order by BOARD_ID desc

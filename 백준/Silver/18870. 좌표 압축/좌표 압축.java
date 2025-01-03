99eb0357f48c9f737700948b00ce63e683a7f6e3
오승현 <75405129+timber3@users.noreply.github.com>
2025-01-03 18:36:23 +0900

    [Silver II] Title: 좌표 압축, Time: 1856 ms, Memory: 261988 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -9,49 +9,29 @@ public class Main {
 
     static int n;
     static int[] arr;
-    static ArrayList<Integer> sortedArr = new ArrayList<>();
-    static HashSet<Integer> set = new HashSet<>();
 
     public static void main(String[] args) throws Exception {
 
         n = Integer.parseInt(br.readLine());
-
         st = new StringTokenizer(br.readLine());
 
         arr = new int[n];
-
-        for (int i = 0 ; i < n ; i ++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            if (!set.contains(arr[i])) {
-                set.add(arr[i]);
-                sortedArr.add(arr[i]);
-            }
         }
 
-        Collections.sort(sortedArr);
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
 
-        for (int i = 0 ; i < n ; i ++) {
+        ArrayList<Integer> sortedArr = new ArrayList<>(set);
 
-            int left = 0;
-            int right = sortedArr.size() - 1;
-
-            int target = arr[i];
-
-            while(left < right) {
-                int mid = (left + right) / 2;
-
-                if (sortedArr.get(mid) >= target) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-
-            sb.append(left).append(' ');
-
+        for (int num : arr) {
+            int index = Collections.binarySearch(sortedArr, num);
+            sb.append(index).append(' ');
         }
 
         System.out.println(sb);
     }
-
 }

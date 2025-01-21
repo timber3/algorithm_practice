56fb7844721964305958894c4eecb4d549fc1e80
오승현 <75405129+timber3@users.noreply.github.com>
2025-01-21 14:11:39 +0900

    [Gold IV] Title: 오큰수, Time: 732 ms, Memory: 208496 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,39 +1,72 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine()); // 7 
-		int [] array = new int [N]; 
-		int [] result = new int [N];
-		Stack<Integer> S = new Stack<>();
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			array[i] = Integer.parseInt(str.nextToken()); //  3 5 2 1 0 4 6 
-			result[i] = -1; // -1 -1 -1 -1 -1 -1 -1
-		}
-		for(int i=0;i<N-1;i++) {
-			if(array[i] < array[i+1]) {
-				result[i] = array[i+1];
-				while(!S.isEmpty()) {
-					if(array[i+1] > array[S.peek()]) {
-						result[S.peek()] = array[i+1];
-						S.pop();
-					}
-					else {
-						break;
-					}
-				}
-			}
-			else {
-				S.push(i);
-			}
-		}
-		for(int i=0;i<N;i++) {
-			sb.append(result[i] + " ");
-		}
-		sb.deleteCharAt(sb.length()-1);
-		System.out.print(sb);
-	}
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n;
+    static int[] arr;
+    static int[] result;
+    static ArrayDeque<Node> q = new ArrayDeque<>();
+
+    static class Node {
+        int val;
+        int idx;
+
+        public Node(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        result = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < n ; i ++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0 ; i < n ; i ++) {
+            result[i] = -1;
+        }
+        // input
+
+        for (int i = 0 ; i < n ; i ++) {
+            // 비어있거나 아니면 꼭대기 값이 더 크면 q에 넣는다.
+            if (q.isEmpty() || q.peekLast().val >= arr[i]) {
+                q.add(new Node(arr[i], i));
+            }
+            // 비어있지 않고 꼭대기 값보다 크다면
+            else {
+                int count = 0;
+                // q가 빌 때 까지 확인하기
+                while(!q.isEmpty()) {
+                    if (q.peekLast().val < arr[i]) {
+                        Node node = q.pollLast();
+                        result[node.idx] = arr[i];
+                    } else {
+                        break;
+                    }
+                }
+                q.add(new Node(arr[i], i));
+            }
+        }
+
+        result[n-1] = -1;
+
+        for (int i = 0 ; i < n ; i++) {
+            sb.append(result[i]).append(' ');
+        }
+
+        System.out.println(sb);
+
+    }
 }

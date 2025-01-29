8870e42917791bc1fffdedc257e1ef74f36e8342
오승현 <75405129+timber3@users.noreply.github.com>
2025-01-29 13:10:54 +0900

    [Gold II] Title: 환승, Time: 1100 ms, Memory: 237372 KB -BaekjoonHub

---
+++ 백준/Gold/5214. 환승/환승.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int n, k, m;
+    static HashSet<Integer>[] map;
+    static int[] visited;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        // 모든 역 + 하이퍼튜브 ( 해당 하이퍼튜브 안에는 어떤 역들이 있는지 나타냄 )
+        // 중복된 역이 들어가는걸 방지하기 위해 set으로 선언
+        map = new HashSet[n + m + 1];
+        visited = new int[n + m + 1];
+
+        for (int i = 1; i <= n + m ; i++) {
+            map[i] = new HashSet<>();
+            visited[i] = -1;
+        }
+
+        for (int i = 1 ; i <= m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            // 하이퍼튜브는 n+1번부터 시작
+            int hyperTube = n + i;
+
+            for (int j = 0; j < k; j++) {
+                int station = Integer.parseInt(st.nextToken());
+                map[station].add(hyperTube);
+                map[hyperTube].add(station);
+            }
+        }
+
+        bfs();
+
+        System.out.println(visited[n]);
+    }
+
+    static void bfs() {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        visited[1] = 1;
+        q.add(1);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : map[cur]) {
+                if (visited[next] == -1) {
+                    // 역을 방문할 때 만 +1 해주기
+                    if (next <= n) {
+                        visited[next] = visited[cur] + 1;
+                    } else {
+                        visited[next] = visited[cur];
+                    }
+                    q.add(next);
+                }
+            }
+        }
+    }
+}

03c024bba079311192d682fe6ee19d0e6b214ec6
오승현 <75405129+timber3@users.noreply.github.com>
2025-01-30 18:09:10 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,17 +1,17 @@
--- 코드를 입력하세요
-# SELECT GU.USER_ID, GU.NICKNAME, sum(GB.PRICE) as TOTAL_SALES
-# FROM USED_GOODS_USER as GU JOIN USED_GOODS_BOARD as GB on GU.USER_ID = GB.WRITER_ID
-# WHERE GB.STATUS = 'DONE' and sum(GB.PRICE) > 700000
-# GROUP BY GB.WRITER_ID
-# ORDER BY TOTAL_SALES;
-
-Select A.WRITER_ID as USER_ID, A.NICKNAME , A.TOTAL_SALES
-From (
-Select GB.WRITER_ID, GU.NICKNAME, SUM(GB.PRICE) as TOTAL_SALES, GB.STATUS
-FROM USED_GOODS_USER as GU JOIN USED_GOODS_BOARD as GB on GU.USER_ID = GB.WRITER_ID
-where GB.STATUS = 'DONE'
-group by GU.USER_ID
-    ) A
-where A.TOTAL_SALES >= 700000
-ORDER BY A.TOTAL_SALES 
-
+select
+    a.user_id, a.nickname, b.total_sales
+from 
+    USED_GOODS_USER a join (
+        select
+            writer_id, sum(price) total_sales
+        from
+            USED_GOODS_BOARD 
+        where
+            status regexp ('DONE')
+        group by
+            writer_id
+        ) b on (a.user_id = b.writer_id)
+where
+    b.total_sales >= 700000
+order by
+    b.total_sales

b3427b034188c7e601409b500558b22d260fdd2a
오승현 <75405129+timber3@users.noreply.github.com>
2025-02-06 13:01:54 +0900

    [Gold V] Title: 선발 명단, Time: 108 ms, Memory: 13160 KB -BaekjoonHub

---
+++ 백준/Gold/3980. 선발 명단/선발 명단.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int T, max;
+    static int[][] map;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+
+        T = Integer.parseInt(br.readLine());
+
+        for (int t = 0 ; t < T ; t ++) {
+            map = new int[11][11];
+            visited = new boolean[11];
+            max = 0;
+
+            for (int i = 0 ; i < 11 ; i ++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0 ; j < 11 ; j ++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dfs(0, 0);
+
+            System.out.println(max);
+        }
+    }
+
+    static void dfs(int depth, int sum) {
+        if (depth == 11) {
+            if (sum > max) {
+                max = sum;
+            }
+            return;
+        }
+
+        if (sum + (11 - depth) * 100 <= max)
+            return;
+
+        for (int i = 0 ; i < 11; i ++) {
+            if (!visited[i] && map[depth][i] != 0) {
+                visited[i] = true;
+                dfs(depth + 1, sum + map[depth][i]);
+                visited[i] = false;
+            }
+        }
+    }
+}

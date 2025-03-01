059f05e1e911ced4256ef0099c60ae3d882761df
오승현 <75405129+timber3@users.noreply.github.com>
2025-03-01 15:58:29 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 84 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+    static int[] arr, operator;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i ++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        operator = new int[4];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0 ; i < 4 ; i ++ ){
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, arr[0]);
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static void dfs(int depth, int sum) {
+        if (depth == n-1) {
+            if (sum > max) max = sum;
+
+            if (min > sum) min = sum;
+            return;
+        }
+
+        if (operator[0] != 0) {
+            operator[0] --;
+            dfs(depth + 1, sum + arr[depth+1]);
+            operator[0] ++;
+        }
+
+        if (operator[1] != 0) {
+            operator[1] --;
+            dfs(depth + 1, sum - arr[depth+1]);
+            operator[1] ++;
+        }
+
+        if (operator[2] != 0) {
+            operator[2] --;
+            dfs(depth + 1, sum * arr[depth+1]);
+            operator[2] ++;
+        }
+
+        if (operator[3] != 0) {
+            operator[3] --;
+            dfs(depth + 1, sum / arr[depth+1]);
+            operator[3] ++;
+        }
+    }
+}

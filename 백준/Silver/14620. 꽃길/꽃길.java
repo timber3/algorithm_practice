c80d934f70a68df009c21d7721300ca3b0958e18
오승현 <75405129+timber3@users.noreply.github.com>
2025-03-08 22:17:49 +0900

    [Silver II] Title: 꽃길, Time: 364 ms, Memory: 20464 KB -BaekjoonHub

---
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int n, min = Integer.MAX_VALUE;
+    static int[][] map;
+    static boolean[][] visited;
+
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0 ; i < n ; i ++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0 ; j < n ; j ++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0);
+
+        System.out.println(min);
+    }
+
+    static void dfs(int depth) {
+        if (depth == 3) {
+            int sum = 0;
+
+            for (int i = 0 ; i < n ; i ++) {
+                for (int j = 0 ; j < n ; j ++) {
+                    if (visited[i][j]) {
+                        sum += map[i][j];
+                    }
+                }
+            }
+
+            if (min > sum) min = sum;
+
+            return;
+        }
+
+        for (int i = 1 ; i < n-1 ; i ++) {
+            for (int j = 1 ; j < n-1 ; j ++) {
+                if (canPlant(i, j) && !visited[i][j]) {
+
+                    visited[i][j] = true;
+                    for (int k = 0 ; k < 4 ; k ++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        visited[nx][ny] = true;
+                    }
+
+                    dfs(depth + 1);
+
+                    visited[i][j] = false;
+                    for (int k = 0 ; k < 4 ; k ++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        visited[nx][ny] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean canPlant(int cx, int cy) {
+        for (int i = 0 ; i < 4 ; i ++) {
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n ) return false;
+
+            if (visited[nx][ny]) return false;
+        }
+
+        return true;
+    }
+
+}

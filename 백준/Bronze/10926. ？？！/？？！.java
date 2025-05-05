7c60af525f1ae444d8811189fd8363cd9af827fb
오승현 <75405129+timber3@users.noreply.github.com>
2025-05-06 02:11:26 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static String str;
+
+    public static void main(String[] args) throws Exception {
+        str = br.readLine();
+
+        System.out.println(str + "??!");
+    }
+}

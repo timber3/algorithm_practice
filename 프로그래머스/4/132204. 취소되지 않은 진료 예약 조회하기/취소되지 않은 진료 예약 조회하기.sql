4f06772a25e210f4332fc22b4c05294b5d619cf1
SEUNGHYEON OH <75405129+timber3@users.noreply.github.com>
2025-05-15 21:37:23 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,6 +1,10 @@
-select APNT_NO, PT_NAME, a.PT_NO, a.MCDP_CD, DR_NAME, APNT_YMD
-from APPOINTMENT a
-join DOCTOR d on a.MDDR_ID = d.DR_ID
-join PATIENT p on a.PT_NO = p.PT_NO
-where APNT_YMD like '2022-04-13%' and a.MCDP_CD = 'CS' and a.APNT_CNCL_YMD IS NULL
-order by APNT_YMD
+# !진료예약번호, !환자이름, !환자번호, !진료과코드, 의사이름, !진료예약일시
+
+
+select APNT_NO, PT_NAME, PT_NO, o1.MCDP_CD, DR_NAME, APNT_YMD
+from (
+    select apnt_no, mddr_id, b.pt_no, b.pt_name, MCDP_CD, apnt_ymd
+    from appointment a join patient b on (a.pt_no = b.pt_no)
+    where APNT_CNCL_YN = 'N' && APNT_YMD like '2022-04-13%'
+) o1 join doctor o2 on (o1.MDDR_ID = o2.DR_ID)
+order by apnt_ymd

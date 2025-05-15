ad1ba4fd5c4189aebb484041d6bcba2a9febe7ba
SEUNGHYEON OH <75405129+timber3@users.noreply.github.com>
2025-05-16 00:00:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -3,5 +3,5 @@
 
 select a.ANIMAL_ID, a.NAME
 from ANIMAL_INS a join ANIMAL_OUTS b on (a.ANIMAL_ID = b.ANIMAL_ID)
-order by datediff(a.DATETIME, b.DATETIME)
+order by b.datetime - a.datetime desc
 limit 2

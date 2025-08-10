d8fae104ab44cfc9c78c1d475402a2e7024363a1
SEUNGHYEON OH <75405129+timber3@users.noreply.github.com>
2025-08-10 15:50:50 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 88 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,40 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-
-    static int n, max;
+    static int N, max;
     static int[] arr, dp;
-
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-
-        arr = new int[n];
-        dp = new int[n];
-
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        dp = new int [N+1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < n ; i ++) {
+        for(int i=1;i <= N ;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = arr[i];
         }
-
-        dp[0] = arr[0];
-        max = arr[0];
-        
-        for (int i = 1 ; i < n ; i ++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[i] > arr[j] && dp[j] + arr[i] > dp[i]) {
-                    dp[i] = dp[j] + arr[i];
+        max= dp[N];
+        for(int i=N  ; i >=1 ; i--) {
+            for(int j = i -1; j >=1 ; j--) {
+                if(arr[i] > arr[j]) {
+                    dp[j] = Math.max(dp[j] , dp[i] + arr[j]);
                 }
             }
+            max = Math.max(max,dp[i]);
 
-            if (dp[i] > max) max = dp[i];
         }
-        
-        System.out.println(max);
+        System.out.print(max);
     }
 }
